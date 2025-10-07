18a037d1fb77b74ccc773547e29086660a29a8ea
𝐀𝐤𝐚𝐬𝐡 𝐃𝐚𝐤𝐬𝐡𝐰𝐚𝐧𝐬𝐡𝐢 : ZoxxOP <akp954834@gmail.com>
2025-10-07 19:04:06 +0530

    Update __init__.py

--- RAUSHAN/__init__.py
+++ RAUSHAN/__init__.py
@@ -39,7 +39,7 @@ async def RAUSHAN():
     header = Table(show_header=True, header_style="bold green")
     header.add_column(LOG_TEXT)
     LOG.print(header)
-    LOG.print(f"[bold cyan]𝐓𝐄𝐀𝐌 𝐏𝐔𝐑𝐕𝐈 𝐁𝐎𝐓𝐒")
+    LOG.print(f"[bold cyan]𝐀𝐍𝐀𝐍𝐘𝐀 𝐁𝐎𝐓𝐒")
     LOG.print("[bold yellow]𝐘𝐨𝐮𝐫 𝐁𝐨𝐭 𝐒𝐭𝐚𝐫𝐭.............")
     await app.start()    
     
