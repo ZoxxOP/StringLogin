eca9535729af676f7678592b22f27f07bfd7d408
𝐀𝐤𝐚𝐬𝐡 𝐃𝐚𝐤𝐬𝐡𝐰𝐚𝐧𝐬𝐡𝐢 : ZoxxOP <akp954834@gmail.com>
2025-10-07 17:12:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,11 @@
-# TEAM PURVI ALL COPYRIGHT ©️
 from pyrogram import filters
 import os
 
 class Config:
-    API_ID = "27079591"
-    API_HASH = "c81ae4c3dc026ea4bf49842a8ce4a5f9"
+    API_ID = "24569721"
+    API_HASH = "b0081b01a3f015d9c76f5ed9e7b20271"
     #TOKEN = "6521122303:AAGCO3XMjcA0SN5NAi1M0NpmbmMxEtwwYbg"
     TOKEN = os.environ.get("TOKEN", None)
-    MONGO_URL = "mongodb+srv://vivek:1234567890@cluster0.c48d8ih.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    MONGO_URL = "mongodb+srv://Akash:SanataniAkash@sanataniakash.co9sxq5.mongodb.net/?retryWrites=true&w=majority&appName=SanataniAkash"
     START_PIC = "https://files.catbox.moe/ppvvg0.jpg"
-    SUDOERS = filters.user(["8143754205"])
+    SUDOERS = filters.user(["8258452998"])
